57bfb6e7ca1688c47fae99078853abb4bfca6a6f
JacobM <jacj@itu.dk>
2021-09-10 14:13:49 +0200

    Solved Generics, Iterators and first Regular Expression exercise

--- Assignment1/Program.cs
+++ Assignment1/Program.cs
@@ -1,12 +1,48 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Assignment1
 {
-    class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
+        }
+
+        public static IEnumerable<T> Flatten<T>(IEnumerable<IEnumerable<T>> lists){
+            foreach (var list in lists)
+            {
+                foreach (var item in list)
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static IEnumerable<T> Filter<T>(IEnumerable<T> items, Predicate<T> predicate){
+            foreach (var item in items)
+            {
+                if (predicate(item))
+                {
+                    yield return item;
+                }
+            }
+        }
+
+        public static bool IsOverHundred(int number) => number > 100 ? true : false;
+
+        public static IEnumerable<string> SplitLine(IEnumerable<string> lines){
+            string pattern = @"[^\s]+";
+            RegexOptions options = RegexOptions.Multiline;
+            foreach (var line in lines)
+            {
+                foreach (Match m in Regex.Matches(line, pattern, options))
+                {
+                    yield return m.Value;
+                }
+            }
         }
     }
 }
